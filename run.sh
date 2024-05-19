46c0fff5b5bbd5fbacfcd007fc6a2205584dba26
naveenkumar <naveen6655@gmail.com>
2024-05-19 18:43:48 +0530

    GoCD pipeline templates

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-if [ "pipeline" == "true" ]; then
+## For template pipeline we will skip the ansible run
+if [ "$pipeline" == "true" ]; then
   exit
 fi
+##
 ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansbile_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
