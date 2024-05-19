6803e46e4c3d547f4920dfa72c177a928b98ba88
naveenkumar <naveen6655@gmail.com>
2024-05-19 18:41:11 +0530

    GoCD pipeline templates

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+if [ "pipeline" == "true" ]; then
+  exit
+fi
 ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansbile_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
