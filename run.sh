0aff01ed5ecd9ccca98184204bc65ad8efa39194
naveenkumar <naveen6655@gmail.com>
2024-05-20 18:10:47 +0530

    changes in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
-### For template pipeline we will skip the ansible run
-#component=$1
-#if [ "$pipeline" == "true" ]; then
-#  exit
-#fi
-###
-##ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansbile_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
-
-env | tee /tmp/file1
+## For template pipeline we will skip the ansible run
+component=$1
+if [ "$pipeline" == "true" ]; then
+  exit
+fi
+##
+ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansbile_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
