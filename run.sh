fbe1cd949eec198de8be71bdef86225f3b16a5a9
naveenkumar <naveen6655@gmail.com>
2024-05-20 18:43:29 +0530

    Changes same as batch79

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 ## For template pipeline we will skip the ansible run
-component=$1
-#if [ "$pipeline" == "true" ]; then
-#  exit
-#fi
+if [ "$pipeline" == "true" ]; then
+  exit
+fi
 ##
-ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansible_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
+ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
