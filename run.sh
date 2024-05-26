94c7217804a18cf8843033cf18bf70bb90d6704a
naveenkumar <naveen6655@gmail.com>
2024-05-26 19:57:58 +0530

    Mysql through pipeline and password encrypt

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ## For template pipeline we will skip the ansible run
-if [ "$pipeline" == "true" ]; then
-  exit
-fi
+#if [ "$pipeline" == "true" ]; then
+#  exit
+#fi
 ##
 ansible-playbook -i ${component}-${env}.devopsengineer2.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
