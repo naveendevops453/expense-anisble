fdda8b203126463e6980cb5d95c21e7ffc73248c
naveenkumar <naveen6655@gmail.com>
2024-05-26 19:20:37 +0530

    devopsengineer2.online updated everywhere

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ "$pipeline" == "true" ]; then
   exit
 fi
 ##
-ansible-playbook -i ${component}-${env}.devopsengineer.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
+ansible-playbook -i ${component}-${env}.devopsengineer2.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
